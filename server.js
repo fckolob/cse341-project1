29d9feca297019ede24f89871f34b7a9d3e9f45f
fckolob <131497920+fckolob@users.noreply.github.com>
2025-09-11 22:44:42 -0300

    added swagger ui express

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const mongodb = require('./routes/data/database');
+swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/', require('./routes'));
 
